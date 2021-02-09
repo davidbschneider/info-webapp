b6e9ded8ea20c7b535859f07fe589a1880a6258b
David <david@blksheep.io>
2021-02-09 18:49:25 +0100

    Improving admin routes, building frontend assets

--- routes/admin.php
+++ routes/admin.php
@@ -6,21 +6,26 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function(){
     return redirect(route('admin.home'));
 });
-// Reset Password
-Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('admin.password.request');
-Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('admin.password.email');
-Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('admin.password.reset');
-Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('admin.password.update');
 
-Route::middleware(['admin.auth'])->group(function () {
-    // Dashboard
-    Route::get('/dashboard', 'HomeController@index')->name('admin.home');
-    // Logout
-    Route::post('logout', 'Auth\LoginController@logout')->name('admin.logout');
-});
+Route::name('admin.')->group(function (){
 
-Route::middleware(['admin.guest'])->group(function () {
-    // Login
-    Route::get('login', 'Auth\LoginController@showLoginForm')->name('admin.login');
-    Route::post('login', 'Auth\LoginController@login');
+    // Reset Password
+    Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+    Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+    Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+    Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+    Route::middleware(['admin.auth'])->group(function () {
+        // Dashboard
+        Route::get('dashboard', 'HomeController@index')->name('home');
+        // Logout
+        Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+    });
+
+    Route::middleware(['admin.guest'])->group(function () {
+        // Login
+        Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+        Route::post('login', 'Auth\LoginController@login');
+    });
+
 });
